3c94bbb24201e9130e7f9ef85d162d4318f750b9
unknown <wliu970@gmail.com>
2023-03-01 20:27:54 +0700

    fix: fix bugs at GetQuestions endpoint

--- services/mainApp/app.js
+++ services/mainApp/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+const router = require("./routes");
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +11,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
