c7eb7bbbc1fdbf467756ed196f14cf9c69203c2e
unknown <wliu970@gmail.com>
2023-03-02 18:05:22 +0700

    feat: add addPost Endpoint

--- services/mainApp/controllers/postController.js
+++ services/mainApp/controllers/postController.js
@@ -29,7 +29,20 @@ class PostController {
         }
     }
 
-
+    static async addPost(request, response, next) {
+        try {
+            const { title, description } = request.body;
+            const post = await Post.create({
+                title,
+                description,
+                UserId: 1, //! ini tegantung sama yang login
+                CategoryId: 1, //! ini tergantung dia pas ngeadd ada dihalaman apa
+            });
+            response.status(201).json(post);
+        } catch (err) {
+            response.status(500).json(err);
+        }
+    }
 }
 
 module.exports = PostController;
--- services/mainApp/routes/post.js
+++ services/mainApp/routes/post.js
@@ -3,5 +3,6 @@ const PostController = require('../controllers/postController')
 const postRouter = express.Router()
 
 postRouter.get('/:categoryId', PostController.getAllPostByCategoryId)
+postRouter.post('/', PostController.addPost)
 
 module.exports = postRouter
