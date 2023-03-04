6c87b2d955286edab7ddb5d104e8ecfcb2639fc8
gilangmgm98@gmail.com <gilangmgm98@gmail.com>
2023-03-04 21:52:12 +0700

    update pull untuk testing 3

--- services/mainApp/__test__/question.test.js
+++ services/mainApp/__test__/question.test.js
@@ -1,32 +1,49 @@
-// const app = require('../app');
-// const request = require('supertest');
-// const { sequelize, Post, Comment, Category } = require('../models');
-// const { queryInterface } = sequelize;
-// const { signToken } = require('../helpers/jwt');
-// const { axios } = require('axios');
+const app = require('../app');
+const request = require('supertest');
+const { sequelize, Post, Comment, Category } = require('../models');
+const { queryInterface } = sequelize;
+const { signToken } = require('../helpers/jwt');
+const { axios } = require('axios');
 
-// let access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkNoeGtkdnRwYVBwbUVLeGsyV1BhIiwiZW1haWwiOiJ0ZXN0aW5nQG1haWwuY29tIiwiaWF0IjoxNjc3ODMwNjAxfQ.ENI_woraRmxzHE8Ay0r1xqWVjpZiy_0W44RPHW59eAU';
-// let invalid_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkNoeGtkdnRwYVBwbUVLeGsyV1BhIiwiZW1haWwiOiJ0ZXN0aW5nQG1haWwuY29tIiwiaWF0IjoxNjc3ODMwNjAxfQ.';
-// let null_token = null
+let access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNWQ25nT0RIMmhtVmJ6UmFtc0pOIiwiZW1haWwiOiJ0ZXN0aW5nQG1haWwuY29tIiwiaWF0IjoxNjc3OTM3MDQ1fQ.7k9P3r7_8jvXHkxw7sFkq5edr-4lkFQb0zzU_uq309E';
+let invalid_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkNoeGtkdnRwYVBwbUVLeGsyV1BhIiwiZW1haWwiOiJ0ZXN0aW5nQG1haWwuY29tIiwiaWF0IjoxNjc3ODMwNjAxfQ.';
+let null_token = null
 
-// describe("GET /questions", () => {
-//     test("should return 200 status code success get question data with categoryValue", (done) => {
-//         request(app)
-//             .get("/questions/9")
-//             .set('access_token', access_token)
-//             .then((response) => {
-//                 // console.log(response);
-//                 const { body, status } = response;
-//                 expect(status).toBe(200);
-//                 expect(body[0]).toHaveProperty("category", expect.any(String));
-//                 expect(body[0]).toHaveProperty("question", expect.any(String));
-//                 expect(body[0]).toHaveProperty("correctAnswer", expect.any(String));
-//                 expect(body[0]).toHaveProperty("incorrectAnswers", expect.any(Array));
-//                 done();
-//             },10000)
-//             .catch((err) => {
-//                 done(err);
-//             });
-//     });
-// });
+describe("GET /questions", () => {
+    test("should return 200 status code success get question data with categoryValue", (done) => {
+        request(app)
+            .get("/questions/9")
+            .set('access_token', access_token)
+            .then((response) => {
+                // console.log(response);
+                const { body, status } = response;
+                expect(status).toBe(200);
+                expect(body[0]).toHaveProperty("category", expect.any(String));
+                expect(body[0]).toHaveProperty("question", expect.any(String));
+                expect(body[0]).toHaveProperty("correctAnswer", expect.any(String));
+                expect(body[0]).toHaveProperty("incorrectAnswers", expect.any(Array));
+                done();
+            },10000)
+            .catch((err) => {
+                done(err);
+            });
+    });
+ 
+    test("should return 401 status code fail because invalid access_token", (done) => {
+        request(app)
+            .get("/questions/9")
+            .set('access_token', invalid_token)
+            .then((response) => {
+                // console.log(response);
+                const { body, status } = response;
+                expect(status).toBe(401);
+                expect(body).toHaveProperty("message", expect.any(String));
+                expect(body.message).toBe("Invalid token");
+                done();
+            },10000)
+            .catch((err) => {
+                done(err);
+            });
+    });
+});
 
