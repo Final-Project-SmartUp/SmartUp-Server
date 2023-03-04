a6ecfd10969c94c8394ad07322f81ae837372a41
fahmiMaulana1337 <fahmimaulana1337@gmail.com>
2023-03-04 16:40:08 +0700

    test User Done

--- services/firebase/routes/rooms.js
+++ services/firebase/routes/rooms.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 
 router.get("/",RoomController.getAllRoomsPlayer2Empty)
-router.post("/createRoom",RoomController.createRoom)
+router.post("/createRoom/:userId",RoomController.createRoom)
 router.get("/:roomId",RoomController.getRoomById)
 router.put("/:roomId",RoomController.joinRoom)
 router.delete("/:roomId",RoomController.deleteRoom)
