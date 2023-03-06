673ddc5f7ac66289e5399250a4f1cafae1afaa15
gilangmgm98@gmail.com <gilangmgm98@gmail.com>
2023-03-06 10:52:12 +0700

    fix after dunkin

--- services/mainApp/__test__/category.test.js
+++ services/mainApp/__test__/category.test.js
@@ -9,92 +9,10 @@ const { axios } = require('axios');
 
 let random = (Math.random() + 1).toString(36).substring(7);
 
-const user = {
-    username: random,
-    email: `${random}@mail.com`,
-    password: "usertest",
-};
 
-let access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkNoeGtkdnRwYVBwbUVLeGsyV1BhIiwiZW1haWwiOiJ0ZXN0aW5nQG1haWwuY29tIiwiaWF0IjoxNjc3ODMwNjAxfQ.ENI_woraRmxzHE8Ay0r1xqWVjpZiy_0W44RPHW59eAU';
+
+let access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNWQ25nT0RIMmhtVmJ6UmFtc0pOIiwiZW1haWwiOiJ0ZXN0aW5nQG1haWwuY29tIiwiaWF0IjoxNjc3OTM3MDQ1fQ.7k9P3r7_8jvXHkxw7sFkq5edr-4lkFQb0zzU_uq309E';
 let invalid_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkNoeGtkdnRwYVBwbUVLeGsyV1BhIiwiZW1haWwiOiJ0ZXN0aW5nQG1haWwuY29tIiwiaWF0IjoxNjc3ODMwNjAxfQ.';
-
-describe("Post /Users", () => {
-    test("should return 201 status success register user", (done) => {
-        request(appUser)
-            .post("/users/register")
-            .send(user)
-            .then((response) => {
-                const { body, status } = response
-                expect(status).toBe(201)
-                expect(body).toHaveProperty("id", expect.any(String))
-                expect(body).toHaveProperty("username", expect.any(String))
-                expect(body).toHaveProperty("email", expect.any(String))
-            })
-            .catch((err) => {
-                done(err);
-            })
-    })
-
-    test("should return 200 status success login user", (done) => {
-        request(appUser)
-            .post("/users/login")
-            .send(user)
-            .then((response) => {
-                const { body, status } = response
-                access_token=body.access_token
-                expect(status).toBe(200)
-                expect(body).toHaveProperty("access_token", expect.any(String))
-                expect(body).toHaveProperty("id", expect.any(String))
-                expect(body).toHaveProperty("username", expect.any(String))
-            })
-            .catch((err) => {
-                done(err);
-            })
-    })
-
-    
-
-
-})
-// describe("Post /Users", () => {
-//     test("should return 201 status success register user", (done) => {
-//         request(appUser)
-//             .post("/users/register")
-//             .send(user)
-//             .then((response) => {
-//                 const { body, status } = response
-//                 expect(status).toBe(201)
-//                 expect(body).toHaveProperty("id", expect.any(String))
-//                 expect(body).toHaveProperty("username", expect.any(String))
-//                 expect(body).toHaveProperty("email", expect.any(String))
-//             })
-//             .catch((err) => {
-//                 done(err);
-//             })
-//     })
-
-//     test("should return 200 status success login user", (done) => {
-//         request(appUser)
-//             .post("/users/login")
-//             .send(user)
-//             .then((response) => {
-//                 const { body, status } = response
-//                 access_token=body.access_token
-//                 expect(status).toBe(200)
-//                 expect(body).toHaveProperty("access_token", expect.any(String))
-//                 expect(body).toHaveProperty("id", expect.any(String))
-//                 expect(body).toHaveProperty("username", expect.any(String))
-//             })
-//             .catch((err) => {
-//                 done(err);
-//             })
-//     })
-
-    
-
-
-// })
-
 describe("GET /categories", () => {
     test("should return 200 status code success get all categories", (done) => {
         request(app)
@@ -102,7 +20,7 @@ describe("GET /categories", () => {
             .set('access_token', access_token)
             .then((response) => {
                 const { body, status } = response;
-                // console.log(body, 'ini dia body si get categories');
+                console.log(body, 'ini dia body si get categories');
                 expect(status).toBe(200);
                 expect(body[0]).toHaveProperty("id", expect.any(Number));
                 expect(body[0]).toHaveProperty("name", expect.any(String));
