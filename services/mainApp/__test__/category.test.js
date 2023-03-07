fb66dbe3a8c6d597b33927204f09e89bad36039f
gilangmgm98@gmail.com <gilangmgm98@gmail.com>
2023-03-07 10:38:09 +0700

    update testing category fix

--- services/mainApp/__test__/category.test.js
+++ services/mainApp/__test__/category.test.js
@@ -1,5 +1,4 @@
 const app = require('../app');
-// const appUser = require('.../firebase/app.js'); 
 const appUser = require(''); 
 const request = require('supertest');
 const { sequelize, Post, Comment, Category } = require('../models');
@@ -7,93 +6,9 @@ const { queryInterface } = sequelize;
 const { signToken } = require('../helpers/jwt');
 const { axios } = require('axios');
 
-let random = (Math.random() + 1).toString(36).substring(7);
-
-const user = {
-    username: random,
-    email: `${random}@mail.com`,
-    password: "usertest",
-};
 
 let access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkNoeGtkdnRwYVBwbUVLeGsyV1BhIiwiZW1haWwiOiJ0ZXN0aW5nQG1haWwuY29tIiwiaWF0IjoxNjc3ODMwNjAxfQ.ENI_woraRmxzHE8Ay0r1xqWVjpZiy_0W44RPHW59eAU';
 let invalid_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkNoeGtkdnRwYVBwbUVLeGsyV1BhIiwiZW1haWwiOiJ0ZXN0aW5nQG1haWwuY29tIiwiaWF0IjoxNjc3ODMwNjAxfQ.';
-
-describe("Post /Users", () => {
-    test("should return 201 status success register user", (done) => {
-        request(appUser)
-            .post("/users/register")
-            .send(user)
-            .then((response) => {
-                const { body, status } = response
-                expect(status).toBe(201)
-                expect(body).toHaveProperty("id", expect.any(String))
-                expect(body).toHaveProperty("username", expect.any(String))
-                expect(body).toHaveProperty("email", expect.any(String))
-            })
-            .catch((err) => {
-                done(err);
-            })
-    })
-
-    test("should return 200 status success login user", (done) => {
-        request(appUser)
-            .post("/users/login")
-            .send(user)
-            .then((response) => {
-                const { body, status } = response
-                access_token=body.access_token
-                expect(status).toBe(200)
-                expect(body).toHaveProperty("access_token", expect.any(String))
-                expect(body).toHaveProperty("id", expect.any(String))
-                expect(body).toHaveProperty("username", expect.any(String))
-            })
-            .catch((err) => {
-                done(err);
-            })
-    })
-
-    
-
-
-})
-// describe("Post /Users", () => {
-//     test("should return 201 status success register user", (done) => {
-//         request(appUser)
-//             .post("/users/register")
-//             .send(user)
-//             .then((response) => {
-//                 const { body, status } = response
-//                 expect(status).toBe(201)
-//                 expect(body).toHaveProperty("id", expect.any(String))
-//                 expect(body).toHaveProperty("username", expect.any(String))
-//                 expect(body).toHaveProperty("email", expect.any(String))
-//             })
-//             .catch((err) => {
-//                 done(err);
-//             })
-//     })
-
-//     test("should return 200 status success login user", (done) => {
-//         request(appUser)
-//             .post("/users/login")
-//             .send(user)
-//             .then((response) => {
-//                 const { body, status } = response
-//                 access_token=body.access_token
-//                 expect(status).toBe(200)
-//                 expect(body).toHaveProperty("access_token", expect.any(String))
-//                 expect(body).toHaveProperty("id", expect.any(String))
-//                 expect(body).toHaveProperty("username", expect.any(String))
-//             })
-//             .catch((err) => {
-//                 done(err);
-//             })
-//     })
-
-    
-
-
-// })
 
 describe("GET /categories", () => {
     test("should return 200 status code success get all categories", (done) => {
